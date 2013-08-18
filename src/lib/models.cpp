94f180ed6fea0d8864554daac57a51175aeb33ed
Reed A. Cartwright <reed@cartwrig.ht>
2013-08-18 09:13:46 +0000

    uniformize_over_rates implementation
    git-svn-id: svn://scit.us/dawg/branches/unirate@751 7e5b8c5c-87e2-0310-a459-8d91125b37d5

--- src/lib/models.cpp
+++ src/lib/models.cpp
@@ -5,6 +5,7 @@
 #include <dawg/subst.h>
 #include <cassert>
 #include <cstdio>
+#include <algorithm>
 
 bool dawg::subst_model::create_alias_tables() {
 	std::size_t sz = model_size();
@@ -12,5 +13,33 @@ bool dawg::subst_model::create_alias_tables() {
 	mutation_table_.resize(sz);
 	for(std::size_t k = 0; k < sz; ++k)
 		mutation_table_[k].create(&table[k][0], &table[k][sz]);
+	return true;
+}
+
+bool dawg::subst_model::uniformize_over_rates(const std::vector<double> &rates) {
+	std::size_t sz = model_size();
+	std::vector<double> work(sz, 0.0);
+	rate_mutation_table_.resize(rates.size());
+
+	// identify max rate
+	double max_rate = *std::max_element(rates.begin(), rates.end());
+	// construct tables
+	for(std::size_t k=0;k<rates.size();++k) {
+		rate_mutation_table_[k].resize(sz);
+		for(std::size_t i=0; i<sz; ++i) {
+			// fill work table for row i and rate k
+			for(std::size_t j=0; j<sz; ++j)
+				work[j] = table[i][j]*uniform_scale()*rates[k];
+			// create more pseudomutations
+			work[i] += uniform_scale()*(max_rate - rates[k]);
+			// rescale
+			for(std::size_t j=0; j<sz; ++j)
+				work[j] /= uniform_scale()*max_rate;
+			// construct sampling table
+			rate_mutation_table_[k][i].create_inplace(work);
+		}
+	}
+	// save scale
+	uni_rate_scale_ = max_rate;
 	return true;
 }
